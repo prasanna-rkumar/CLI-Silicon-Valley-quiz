ed919e040dbd04f7322bf70941f667a2b6316892
Prasanna Kumar <34109342+prasanna-rkumar@users.noreply.github.com>
2021-04-11 23:41:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,62 +4,62 @@ const rs = require('readline-sync')
 var score = 0;
 const questions = [
   {
-    q: "What is the current engine size of MotoGP bikes?",
-    options: "a: 600cc, b: 500cc, c: 1000cc",
-    a: "c",
-  },
-  {
-    q: "Which MotoGP circuit is known as ‘The Cathedral’?",
-    options: "a: Assen, b: Mugello, c: Motegi",
+    q: "What is Pied Piper?",
+    options: "a: Lossless Compression software, b: A song, c: A scary story",
     a: "a",
   },
   {
-    q: "Who was the last Japanese rider to win the MotoGP title?",
-    options: "a: Naoki Matsudo, b: Tetsuya Harada, c: None",
-    a: "c",
-  },
-  {
-    q: "Which sweet company famously sponsored Jorge Lorenzo?",
-    options: "a: Chupa Chups, b: Cadbury's, c: Haribo",
+    q: "Gavin Belson originally offered how much to buy Pied Piper?",
+    options: "a: $600,000, b: $1,000,000, c: $150,000,000",
     a: "a",
   },
   {
-    q: "Valentino Rossi shocked MotoGP fans by winning his first race for Yamaha after switching over from Honda. What year was that?",
-    options: "a: 1999, b: 2010, c: 2004",
-    a: "c",
+    q: "At TechCrunch Disrupt, Jared was on what drug?",
+    options: "a: Weed, b: Adderall, c: Opiates",
+    a: "b",
   },
   {
-    q: "Which of these riders has never driven a Formula One car?",
-    options: "a: Valentino Rossi, b: Casey Stoner, c: Giacomo Agostini",
-    a: "c",
-  },
-  {
-    q: "Who’s the most successful Grand Prix rider of all time in terms of wins?",
-    options: "a: Giacomo Agostini, b: VAlentino Rossi, c: Marc Márquez",
+    q: "What bank chain was Carver notorious for hacking in the past?",
+    options: "a: Bank of America, b: Chase Bank, c: Standard Bank",
     a: "a",
   },
   {
-    q: "What ritual does Valentino Rossi always perform before he gets on his bike?",
-    options: "a: Holds right footpeg, b: Kisses brake lever, c: walks around it thrice",
+    q: "How did Erlich originally come to own 10% of Pied Piper?",
+    options: "a: Incubation policy, b: He bought it, c: He tricked Richard",
     a: "a",
   },
   {
-    q: "Who was the youngest ever MotoGP world champion?",
-    options: "a: Marc Márquez, b: Valentino Rossi, c: Maverick Viñales",
-    a: "c",
+    q: "People often incorrectly believe Jared has what type of health issue?",
+    options: "a: Wasting disease, b: Substance use disorder, c: Thyroid",
+    a: "a",
   },
   {
-    q: "Casey Stoner was a two-time MotoGP champion in 2007 and 2010. But what discipline had he competed in prior to taking up road racing?",
-    options: "a: Australian V8, b: Formula Three, c: Dirt track",
-    a: "c",
+    q: "Gilfoyle was an undocumented immigrant from what country?",
+    options: "a: Mexico, b: Canada, c: Germany",
+    a: "b",
+  },
+  {
+    q: "Gavin said he hired which famous singer to preform at TechCrunch?",
+    options: "a: Taylor Swift, b: Shakira, c: Justin Beiber",
+    a: "b",
+  },
+  {
+    q: "Where did Richard first meet Peter Gregory?",
+    options: "a: Outside Ted Talk, b: Coffee Shop, c: At Hooli",
+    a: "a",
+  },
+  {
+    q: "What is the name of the test that measures the performance of lossless compression software?",
+    options: "a: Weissman, b: The Compressor, c: Disruptor",
+    a: "a",
   },
 ];
 
 const validOptions = ['a', 'b', 'c'];
 
 function askQuestion(question) {
-  console.log(chalk.yellow(question.q));
-  const answer = rs.question(chalk.cyan(question.options));
+  console.log(chalk.yellow(question.q), "\n");
+  const answer = rs.question(chalk.cyan(question.options + "\n"));
   if (validOptions.includes(answer)) {
     if (answer == question.a) {
       console.log(chalk.green("That is correct!!"));
@@ -74,12 +74,12 @@ function askQuestion(question) {
   console.log("\n")
 }
 
-console.log(chalk.green("Welcome to MotoGP quiz!!"));
+console.log(chalk.green("Welcome to Silicon Valley quiz!!"));
 
 var userName = rs.question("What's Your name? ");
 
 console.log("Hi " + userName + ", Let's get started\n");
-console.log("We will ask you a set of 10 questions about MotoGP.", chalk.cyan(" \nType the correct option E.g: ", chalk.hex('#ff00ff').bold("'a, b or c'")));
+console.log("We will ask you a set of 10 questions about Silicon Valley.", chalk.cyan(" \nType the correct option E.g: ", chalk.hex('#ff00ff').bold("'a, b or c'")));
 
 for (var i = 0; i < questions.length; i++) {
   askQuestion(questions[i]);
